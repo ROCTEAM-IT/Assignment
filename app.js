e7342e5031b806ffb9a67112090bdf5cef171bb5
NgDK27 <85181668+NgDK27@users.noreply.github.com>
2022-01-14 10:56:12 +0700

    Fixed buttons

--- app.js
+++ app.js
@@ -10,3 +10,10 @@ document.addEventListener('scroll', () => {
 		header.style.backgroundColor = 'transparent';
 	}
 })
+
+let more = document.querySelectorAll(".more");
+for (let i = 0; i < more.length; i++) {
+	more[i].addEventListener("click", function () {
+	more[i].parentNode.classList.toggle("active");
+	});
+}
